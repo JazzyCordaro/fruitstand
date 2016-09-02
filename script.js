bc8ddf43158232ec88d6bf7b1c8525f1032118c8
Nate Biessener <nathaniel.biessener@gmail.com>
2016-09-02 09:32:28 -0500

    comments and sell button fix

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 console.log('sourced');
-
+//count starts at 15 in html file, setting to 14 here will start the decrementing on the first interval
 var count = 14;
+var totalMonies = 100;
+var inventory = [];
+//set random initial price for each fruit
 var fruitsForSale = {
   banana: {price: (Math.floor(Math.random() * 950) + 50)/100},
   apple: {price: (Math.floor(Math.random() * 950) + 50)/100},
   grape: {price: (Math.floor(Math.random() * 950) + 50)/100},
   orange: {price: (Math.floor(Math.random() * 950) + 50)/100}
 };
-var totalMonies = 100;
-var inventory = [];
 
+//update DOM with initial fruit prices
 $(document).ready(function(){
   for (var fruit in fruitsForSale) {
     $('#' + fruit + 'Price').html(fruitsForSale[fruit].price.toLocaleString('USD', {style: 'currency', currency: "USD"}));
+    $('#sell' + fruit.charAt(0).toUpperCase() + fruit.slice(1)).addClass('btn-danger');
+    $('#sell' + fruit.charAt(0).toUpperCase() + fruit.slice(1)).html('Can\'t Sell');
   }
-});
+  //change sell buttons to can't sell
+});//end doc ready
 
+//every second, update the count, the prices, and the display
 var interval = setInterval(function(){
   console.log('in set interval');
   $('#countDown').html(':' + count);
@@ -27,61 +33,41 @@ var interval = setInterval(function(){
     //update display
   } else {
     count--;
-
-  }
-}, 1000);
-
-var updatePrices = function () {
-  //go through each oject and change price property by random 0-.50
-  for (var fruit in fruitsForSale) {
-    var randomChange = (Math.random() /2 + 0.01).toFixed(2);
-    randomChange = Number(randomChange);
-    if (Math.random() > .5){
-      randomChange *= -1;
-    }
-    fruitsForSale[fruit].price += randomChange;
-    if(fruitsForSale[fruit].price < .50){
-      fruitsForSale[fruit].price = .50;
-    }else if(fruitsForSale[fruit].price > 9.99){
-      fruitsForSale[fruit].price = 9.99;
-    }
-    console.log(fruitsForSale[fruit].price);
-
-  }
-
-};//end updatePrices
+  }//end if/else
+}, 1000);//end interval
 
 var buyFruit = function (fruit){
-    //if not enough money can't buyFruit
+  //if not enough money can't buyFruit
   if ( totalMonies < fruitsForSale[fruit].price){
-    //----------------need error
-
-  }
-    //else deduct money, add fruit
-    else {
+    //no action
+  }//end if
+  //else deduct money, add fruit to inventory
+  else {
     totalMonies -= fruitsForSale[fruit].price;
+    //push fruit object to inventory
     inventory.push({
       name: fruit,
       price: fruitsForSale[fruit].price,
-    });
+    });//end push
     //if money is less than .50 stop interval
     if (totalMonies < 0.50 ){
+      //-----------------------------stop loop---still necessary with sell button?
       clearInterval(interval);
       alert('NO MORE MONIES!!!');
-    }
-    //call display and calculate
+    }//end if
+    //call displayFruit
     displayFruit();
-  }
+  }//end else
+};//end buyFruit
 
-};
-
+//calculates inventory $$ amounts, updates full display
 var displayFruit = function () {
   //create individual fruit arrays
   var apples = [];
   var bananas = [];
   var grapes = [];
   var oranges = [];
-
+  //filter inventory into individual arrays
   for (var i = 0; i < inventory.length; i++) {
     switch (inventory[i].name) {
       case 'apple':
@@ -98,7 +84,7 @@ var displayFruit = function () {
         break;
     }//close switch
   }//close for
-  // calculate average price of each type of fruit
+  // calculate total price of each type of fruit
   var appleTotalPrice = 0;
   for (var i = 0; i < apples.length; i++) {
     appleTotalPrice += apples[i].price;
@@ -115,21 +101,22 @@ var displayFruit = function () {
   for (var i = 0; i < oranges.length; i++) {
     orangeTotalPrice += oranges[i].price;
   }
+  // calculate average price of each type of fruit
   var appleAveragePrice = appleTotalPrice / apples.length;
   var bananaAveragePrice = bananaTotalPrice / bananas.length;
   var grapeAveragePrice = grapeTotalPrice / grapes.length;
   var orangeAveragePrice = orangeTotalPrice / oranges.length;
-  //update totalMonies
-  //convert to USD
-  //display totalMonies
+  //end calculations
+  //start display updates
+  //convert totalMonies to USD and display
   $('#showMoney').html(totalMonies.toLocaleString('USD', {style: 'currency', currency: "USD"}));
   //update new prices of fruitsForSale
   $('#applePrice').html(fruitsForSale.apple.price.toLocaleString('USD', {style: 'currency', currency: "USD"}));
   $('#orangePrice').html(fruitsForSale.orange.price.toLocaleString('USD', {style: 'currency', currency: "USD"}));
   $('#bananaPrice').html(fruitsForSale.banana.price.toLocaleString('USD', {style: 'currency', currency: "USD"}));
   $('#grapePrice').html(fruitsForSale.grape.price.toLocaleString('USD', {style: 'currency', currency: "USD"}));
-  //calculate how many of each fruit they have .length and display
-  // #[fruitCount] = fruitArray.length
+  //end fruit price update
+  // create singluar/plural count string for each fruit and add to DOM
   var appleCount = apples.length;
   var bananaCount = bananas.length;
   var grapeCount = grapes.length;
@@ -162,11 +149,13 @@ var displayFruit = function () {
   else {
     orangeCount += ' oranges';
   }
+
   $('#appleCount').html(appleCount);
   $('#bananaCount').html(bananaCount);
   $('#grapeCount').html(grapeCount);
   $('#orangeCount').html(orangeCount);
-  // #[averageFruit] = fruitAveragePrice
+  //end fruit count update
+  // format average price to USD and add to DOM
   if (appleAveragePrice > 0){
     $('#averageApple').html(appleAveragePrice.toLocaleString('USD', {style: 'currency', currency: "USD"}));
   }
@@ -191,33 +180,90 @@ var displayFruit = function () {
   else {
     $('#averageOrange').html('$0.00');
   }
-  // #[totalFruit] = fruitTotalPrice
+  //end average price update
+  //format total spent on each fruit to USD and add to DOM
   $('#totalApple').html(appleTotalPrice.toLocaleString('USD', {style: 'currency', currency: "USD"}));
   $('#totalBanana').html(bananaTotalPrice.toLocaleString('USD', {style: 'currency', currency: "USD"}));
   $('#totalGrape').html(grapeTotalPrice.toLocaleString('USD', {style: 'currency', currency: "USD"}));
   $('#totalOrange').html(orangeTotalPrice.toLocaleString('USD', {style: 'currency', currency: "USD"}));
-  //display
-  //calculate total price of each fruit in inventory
-
+  //end total price update
+  //update buttons depending on whether user can afford each fruit
   for (var fruit in fruitsForSale) {
     if (fruitsForSale[fruit].price > totalMonies) {
       $('#buy' + fruit.charAt(0).toUpperCase() + fruit.slice(1)).addClass('btn-danger');
       $('#buy' + fruit.charAt(0).toUpperCase() + fruit.slice(1)).html('Can\'t Buy');
-    }
+    }//end if
     if (fruitsForSale[fruit].price < totalMonies) {
       $('#buy' + fruit.charAt(0).toUpperCase() + fruit.slice(1)).removeClass('btn-danger');
       $('#buy' + fruit.charAt(0).toUpperCase() + fruit.slice(1)).html('Buy!');
-    }
-  }
-};
+    }//end if
+  }//end for
+  //update sale buttons
+  if (apples.length > 0) {
+    $('#sellApple').removeClass('btn-danger');
+    $('#sellApple').html('Sell!');
+  }//end if
+  else {
+    $('#sellApple').addClass('btn-danger');
+    $('#sellApple').html('Can\'t Sell');
+  }//end else
+  if (bananas.length > 0) {
+    $('#sellBanana').removeClass('btn-danger');
+    $('#sellBanana').html('Sell!');
+  }//end if
+  else {
+    $('#sellBanana').addClass('btn-danger');
+    $('#sellBanana').html('Can\'t Sell');
+  }//end else
+  if (grapes.length > 0) {
+    $('#sellGrape').removeClass('btn-danger');
+    $('#sellGrape').html('Sell!');
+  }//end if
+  else {
+    $('#sellGrape').addClass('btn-danger');
+    $('#sellGrape').html('Can\'t Sell');
+  }//end else
+  if (oranges.length > 0) {
+    $('#sellOrange').removeClass('btn-danger');
+    $('#sellOrange').html('Sell!');
+  }//end if
+  else {
+    $('#sellOrange').addClass('btn-danger');
+    $('#sellOrange').html('Can\'t Sell');
+  }//end else
+  //end button update
+};//end displayFruit
 
 var sellFruit = function(fruit){
+  //start searching inventory for fruit
   for (var i = 0; i < inventory.length; i++) {
     if(inventory[i].name === fruit){
+      //once first fruit is found, remove from array, add sale price to totalMonies, update display, then break out of the loop
       inventory.splice(i, 1);
       totalMonies += fruitsForSale[fruit].price;
       displayFruit();
       break;
-    }
-  }
-};
+    }//end if
+  }//end for
+};//end sellFruit
+
+var updatePrices = function () {
+  //go through each oject and change price property by random 0-.50
+  for (var fruit in fruitsForSale) {
+    //random change amt, 0.00... to 0.99..., divided by 2 to get 0.00 - 0.49..., + .01, to 2 digits
+    var randomChange = (Math.random() /2 + 0.01).toFixed(2);
+    //make it a number
+    randomChange = Number(randomChange);
+    //coin flip to determine + or -
+    if (Math.random() > .5){
+      randomChange *= -1;
+    }//end if
+    fruitsForSale[fruit].price += randomChange;
+    if(fruitsForSale[fruit].price < .50){
+      fruitsForSale[fruit].price = .50;
+    }else if(fruitsForSale[fruit].price > 9.99){
+      fruitsForSale[fruit].price = 9.99;
+    }//end if/else if
+    // console.log(fruitsForSale[fruit].price);
+  }//end for
+};//end updatePrices
